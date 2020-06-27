3267c8026851a91d1afe1163c4e37a0ffcbc9e68
nikit34 <permikov134@mail.ru>
2020-06-28 00:22:27 +0500

    add Camera

--- move_counter.py
+++ move_counter.py
@@ -14,29 +14,31 @@ class Data:
         file = open(r'buffer.txt', 'r')
         return file.read()
 
+
+class Camera(Data):
+    def __init__(self, env):
+        self.env = env
+        self.cap = cv2.VideoCapture(self.read_file())
+        super(Data).__init__(env)
+
     def get_param_camera(self):
-        cap = cv2.VideoCapture(self.read_file())
-        frames_count = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-        fps = cap.get(cv2.CAP_PROP_FPS)
-        width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-        height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-        return [frames_count, fps, width, height]
+        params = {
+            'frames_count': self.cap.get(cv2.CAP_PROP_FRAME_COUNT),
+            'fps': self.cap.get(cv2.CAP_PROP_FPS),
+            'width': self.cap.get(cv2.CAP_PROP_FRAME_WIDTH),
+            'height': self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT),
+        }
+        return params
 
-    def __str__(self):
-        return self.env
-
-
-
-
-df = pd.DataFrame()
-df.index.name = "Frames"
-
-frame_number = 0
-obj_cross_up = 0
-obj_cross_down = 0
-obj_id = []
-obj_crossed = []
-obj_total = 0
+class Statistic:
+    def __init__(self):
+        self.df = pd.DataFrame()
+        self.frame_number = 0
+        self.obj_cross_up = 0
+        self.obj_cross_down = 0
+        self.obj_id = []
+        self.obj_crossed = []
+        self.obj_total = 0
 
 back_bg = cv2.createBackgroundSubtractorMOG2()
 
@@ -45,5 +47,5 @@ ratio = .5
 image = cv2.resize(frame, (0, 0), None, ratio, ratio)
 
 if __name__ == 'main':
-    Data('local')
+
 
